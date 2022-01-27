d10fb77fc85c3bd16a413fce4b4352f375df737f
Eliud Heredia <eliudheredia@Eliuds-MBP.fios-router.home>
2022-01-27 15:54:34 -0500

    HW1

--- controllers/TuitController.ts
+++ controllers/TuitController.ts
@@ -10,20 +10,12 @@ export default class TuitController implements TuitControllerI {
         this.tuitDao = tuitDao;
         this.app.get('/tuits', this.findAllTuits);
         this.app.get('./tuits/:tid', this.findTuitById);
+        this.app.get('./users/:uid/tuits', this.findTuitsByUser);
         this.app.post('./tuits', this.createTuit);
         this.app.put('./tuits/:tid', this.updateTuit);
         this.app.delete('./tuits/:tid', this.deleteTuit);
-        this.app.get('./users/:uid/tuits', this.findTuitsByUser);
 
     }
-    createTuit = (req: Request, res: Response) =>
-        this.tuitDao.createTuit(req.body)
-            .then(tuit => res.json(tuit));
-
-    deleteTuit = (req: Request, res: Response) =>
-        this.tuitDao.deleteTuit(req.params.tuitId)
-            .then(status => res.send(status));
-
     findAllTuits = (req: Request, res: Response) =>
         this.tuitDao.findAllTuits()
             .then(tuits => res.json(tuits));
@@ -32,10 +24,17 @@ export default class TuitController implements TuitControllerI {
         this.tuitDao.findTuitById(req.params.tid)
             .then(tuit => res.json(tuit));
 
-
     findTuitsByUser = (req: Request, res: Response) =>
         this.tuitDao.findTuitsByUser(req.params.uid)
             .then(tuits => res.json(tuits));
+
+    createTuit = (req: Request, res: Response) =>
+        this.tuitDao.createTuit(req.body)
+            .then(tuit => res.json(tuit));
+
+    deleteTuit = (req: Request, res: Response) =>
+        this.tuitDao.deleteTuit(req.params.tuitId)
+            .then(status => res.send(status));
 
     updateTuit = (req: Request, res: Response) =>
         this.tuitDao.updateTuit(req.params.tid, req.body)
--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 import User from '../models/User'
 const TuitSchema = new mongoose.Schema({
     username: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
     profilePhoto: String,
     post: String,
     likes: Number,
     hashTag: String,
     bookMark: String,
-    date: {type: Date, default: Date.now},
+    date: String,
 }, {collection: 'tuits'});
 export default TuitSchema;
