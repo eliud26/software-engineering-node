a7afe8fadab414369dd52bb00d0830232eb1acf2
eliudheredia <edu.heredia@ymail.com>
2022-01-28 19:05:41 -0500

    package

--- server.ts
+++ server.ts
@@ -4,5 +4,8 @@ const app = express();
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
 
+// require('./controllers/TuitController')(app);
+// require('./controllers/UserController')(app);
+
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
